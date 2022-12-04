b78088643637d4e8624c1e9d03603f9e8dabec19
Changyuan Lin <coreballium@outlook.com>
2022-12-03 23:50:25 -0500

    Display static marker at bottle origin

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
-import Map, { Layer, Source } from "react-map-gl";
+import Map, { Layer, Marker, Source } from "react-map-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
-import { Bottle } from "./types";
+import { Bottle, GetBottlesResponse, Point, Route } from "./types";
 
 const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 const BASE_URL = "https://bottlz.azurewebsites.net";
+const BOTTLE_SPEED = 1.0;
 const COLORS = [
   "#FF0000",
   "#FFFF00",
@@ -37,8 +38,85 @@ function App() {
   useEffect(() => {
     fetch(`${BASE_URL}/bottles/getAll`)
       .then((res) => res.json())
-      .then((json) => setBottles(json.bottles));
+      .then((json: GetBottlesResponse) =>
+        setBottles(
+          json.bottles.map((bottle) => ({
+            ...bottle,
+            created: new Date(bottle.created),
+          }))
+        )
+      );
   }, []);
+
+  /** The current interpolated location of a bottle */
+  const currentLocation = (bottle: Bottle, date: Date): Point => {
+    if (bottle.routes.length === 0) {
+      return bottle.origin.coordinates;
+    }
+
+    const coveredDistance =
+      (date.getTime() - bottle.created.getTime()) * BOTTLE_SPEED;
+    let accumulatedDistance = 0;
+    let currentRouteIndex = bottle.routes.length - 1;
+    for (let i = 0; i < bottle.routes.length; i++) {
+      if (accumulatedDistance + bottle.routes[i].distance > coveredDistance) {
+        currentRouteIndex = i;
+        break;
+      }
+      accumulatedDistance += bottle.routes[i].distance;
+    }
+
+    const currentRouteTraveledDistance = coveredDistance - accumulatedDistance;
+    const currentRouteFraction =
+      currentRouteTraveledDistance / bottle.routes[currentRouteIndex].distance;
+    return routePosition(
+      bottle.routes[currentRouteIndex],
+      currentRouteFraction
+    );
+  };
+
+  /** The interpolated position that is fraction through a route */
+  const routePosition = (route: Route, fraction: number): Point => {
+    if (fraction > 1) {
+      return route.route[route.route.length - 1];
+    } else if (fraction < 0) {
+      return route.route[0];
+    }
+
+    const edgeLengths: number[] = [];
+    for (let i = 1; i < route.route.length; i++) {
+      const dLat = route.route[i][1] - route.route[i - 1][1];
+      const dLon = route.route[i][0] - route.route[i - 1][0];
+      const length = Math.sqrt(dLat * dLat + dLon * dLon);
+      edgeLengths.push(length);
+    }
+
+    const totalLength = edgeLengths.reduce((a, b) => a + b);
+    const coveredLength = fraction * totalLength;
+
+    let accumulatedLength = 0;
+    let currentEdgeIndex = 0;
+    for (let i = 0; i < edgeLengths.length; i++) {
+      if (accumulatedLength + edgeLengths[i] > coveredLength) {
+        currentEdgeIndex = i;
+        break;
+      }
+      accumulatedLength += edgeLengths[i];
+    }
+
+    const currentEdgeTraveledLength = coveredLength - accumulatedLength;
+    const currentEdgeFraction =
+      currentEdgeTraveledLength / edgeLengths[currentEdgeIndex];
+
+    const currentPoint = route.route[currentEdgeIndex];
+    const nextPoint = route.route[currentEdgeIndex + 1];
+
+    const interpolatedLatitude =
+      currentPoint[1] + (nextPoint[1] - currentPoint[1]) * currentEdgeFraction;
+    const interpolatedLongitude =
+      currentPoint[0] + (nextPoint[0] - currentPoint[0]) * currentEdgeFraction;
+    return [interpolatedLongitude, interpolatedLatitude];
+  };
 
   const routeCollection: GeoJSON.FeatureCollection = {
     type: "FeatureCollection",
@@ -72,6 +150,12 @@ function App() {
             paint={{ "line-width": 3, "line-color": ["get", "color"] }}
           />
         </Source>
+        {bottles.map((bottle) => {
+          const location = bottle.origin.coordinates;
+          return (
+            <Marker longitude={location[0]} latitude={location[1]}></Marker>
+          );
+        })}
       </Map>
     </Box>
   );
